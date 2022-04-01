48996689721eccb7ba45e237c2e689c3bc987175
akib1162100 <35798840+akib1162100@users.noreply.github.com>
2022-03-31 22:21:53 -0700

    added time series plots of all trends

--- explore.py
+++ explore.py
@@ -1,6 +1,17 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 trends_df = pd.read_csv('trends1.csv')
 trends_df = trends_df.loc[:, ~trends_df.columns.str.contains('^Unnamed')] #dropping innamed columns 
-print(trends_df.head(5))
+
+
+def plot_df(df, x, y1,y2,y3,title="", xlabel='Date', ylabel='Value', dpi=100):
+    plt.figure(figsize=(16,5), dpi=100)
+    plt.plot(x, y1, color='tab:red')
+    plt.plot(x, y2, color='tab:blue')
+    plt.plot(x, y3, color='tab:green')
+    plt.gca().set(title=title, xlabel=xlabel, ylabel=ylabel)
+    plt.show()
+
+plot_df(trends_df, x=trends_df['date'], y1=trends_df['top 1'],y2=trends_df['top 2'],y3=trends_df['top 3'], title='trands of all.')
